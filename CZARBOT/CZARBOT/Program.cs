ee4498a724896e2c0f746f87caea72098637fdab
vuoto <binxieboy666@gmail.com>
2018-09-18 00:14:10 -0400

    base

---
+++ CZARBOT/CZARBOT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace CZARBOT
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
